d7d5f2415b84de5930588a23d58a6fe26419eaf7
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2021-02-04 18:50:03 +0530

    Updated Color File with Hex ClassMethod

--- color.py
+++ color.py
@@ -1,4 +1,13 @@
 from vector import Vector
 
 class Color(Vector):
-    pass
+    """
+    Stores colors as RGB triplets.An ALias for Vector
+    """
+    @classmethod
+    def from_hex(cls, hexcolor = "#000000"):
+        x = int(hexcolor[1:3], 16)/255.0
+        x = int(hexcolor[3:5], 16)/255.0
+        x = int(hexcolor[5:7], 16)/255.0
+        return cls(x, y, z)
+
